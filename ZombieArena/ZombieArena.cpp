fde2c9db546571eef6606b8217ae4f46d11694ae
Priyanus Nayak <nayakpriyansu385@gmail.com>
2025-05-07 17:47:39 +0530

    Updated

--- ZombieArena/ZombieArena.cpp
+++ ZombieArena/ZombieArena.cpp
@@ -1,8 +1,13 @@
 #include<SFML/Graphics.hpp>
+#include<cmath>
 #include "player.h"
 using namespace sf;
 
 int main() {
+	enum class State{PAUSED,PLAYING,GAME_OVER,LEVELING_UP};
+	State state = State::GAME_OVER;
+	int wave = 0;
+
 	Vector2f resolution;
 	resolution.x = VideoMode::getDesktopMode().width;
 	resolution.y = VideoMode::getDesktopMode().height;
@@ -11,33 +16,91 @@ int main() {
 
 	View mainview(FloatRect(0, 0, resolution.x, resolution.y));
 
+	Vector2f mouseWorldPosition;
+	Vector2i mouseScreenPosition;
+	window.setMouseCursorVisible(true);
+
 	Player player;
 	IntRect arena;
 
-	arena.left = 0;
-	arena.top = 0;
-	arena.height = 500;
-	arena.width = 500;
-	int tilesize = 50;
-
-	player.spawn(arena, resolution, tilesize);
-
+	
+	Clock clock;
 	while (window.isOpen()) {
 		Event event;
 		while (window.pollEvent(event)) {
 			if (event.type == Event::Closed) {
 				window.close();
 			}
+			if (event.type == Event::KeyPressed) {
+				if (event.key.code == Keyboard::Return && state == State::PLAYING) {
+					state = State::PAUSED;
+				}
+				else if(event.key.code == Keyboard::Return && state == State::PAUSED) {
+					state = State::PLAYING;
+					clock.restart();
+				}
+				else if(event.key.code == Keyboard::Return && state == State::GAME_OVER) {
+					state = State::LEVELING_UP;
+				}
+			}
 		}
 		if (Keyboard::isKeyPressed(Keyboard::Escape)) {
 			window.close();
-		}
+		}IntRect arena;
+		arena.left = 0;
+		arena.top = 0;
+		arena.height = 500;
+		arena.width = 500;
+		int tilesize = 50;
+		player.spawn(arena, resolution, tilesize);
 		
-		mainview.setCenter(player.getCenter());
+		if (state == State::LEVELING_UP) {
+			if (event.key.code == Keyboard::Num1) {
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num2) {
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num3) {
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num4) {
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num5) {
+				state = State::PLAYING;
+			}
+			if (event.key.code == Keyboard::Num6) {
+				state = State::PLAYING;
+			}
+			if (state == State::PLAYING) {
+				wave++;
+				arena.left = 0;
+				arena.top = 0;
+				arena.width = 500;
+				arena.height = 500;
+				int tilesize = 50;
+				player.spawn(arena, resolution, tilesize);
+				clock.restart();
+			}
+		}
+		clock.restart();
+		if (state == State::PLAYING) {
+			Time dt = clock.restart();
+			float dtAsSeconds = dt.asSeconds();
+			mouseScreenPosition = Mouse::getPosition();
+			mouseWorldPosition = window.mapPixelToCoords(Mouse::getPosition(), mainview);
+			player.update(dtAsSeconds, Mouse::getPosition());
+			Vector2f playerPosition(player.getCenter());
+			// Set the main view to the player's current position (center)
+			mainview.setCenter(player.getCenter());
 
-		window.clear();
-		window.setView(mainview);
-		window.draw(player.getSprite());
+		}
+		if (state == State::PLAYING) {
+			window.clear();
+			window.setView(mainview);
+			window.draw(player.getSprite());
+		}
 		window.display();
 	}
 	return 0;
